836361e192459fc7437d926a29596b398cc78cd5
yousaf-qureshi <147919925+yousaf-qureshi@users.noreply.github.com>
2023-10-15 16:14:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+const currentTime = document. querySelector("h1"),
+content=document.querySelector(".content"),
+ selecMenu = document. querySelectorAll("select"),
+ setAlarmBtn=document. querySelector("button");
+
+ let alarmTime, isAlarmSet= false,
+ ringtone= new Audio("./files/ringtone.mp3");
+
+for (let i = 12; i > 0; i--) {
+    i = i <10 ? "0" +  i  : i ;
+    let option=  `<option value="${i}">${i}</option>`;
+    selecMenu[0].firstElementChild.insertAdjacentHTML("afterend",option);
+}
+
+for (let i = 59; i >= 0; i--) {
+    i = i <10 ? "0" +  i  : i ;
+    let option=  `<option value="${i}">${i}</option>`;
+    selecMenu[1].firstElementChild.insertAdjacentHTML("afterend",option);
+}
+
+for (let i = 2; i > 0; i--) {
+    let ampm=i == 1 ? "AM":"PM";
+    let option=  `<option value="${ampm}">${ampm}</option>`;
+    selecMenu[2].firstElementChild.insertAdjacentHTML("afterend",option);
+}
+
+setInterval(() =>{
+    let date = new Date(),
+    h = date.getHours(),
+    m = date.getMinutes(),
+    s = date.getSeconds(),
+    ampm = "AM";
+    if(h >= 12) {
+        h = h - 12;
+        ampm = "PM";
+    }
+    h = h == 0 ? h = 12 : h;
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+    if(alarmTime == `${h}:${m} ${ampm}`){
+      ringtone.play();
+      ringtone.loop=true;
+    }
+
+}, 1000);
+function setAlarm(){
+            if(isAlarmSet){
+                alarmTime="";
+                ringtone.pause();
+                content.classList.remove("disable");
+                setAlarmBtn.innerText="Set Alarm"
+                return  isAlarmSet=false;   
+            }
+            // return isAlarmSet value to false
+
+    // getting hours ,minutes ,ampm select tag value
+    let time=`${ selecMenu[0].value}:${ selecMenu[1].value} ${ selecMenu[2].value}`;
+  if( time.includes("Hour")|| time.includes("Minute")||time.includes("AM/PM")){
+    return alert("Please,select a valid time to Alarm!")
+  }
+  isAlarmSet=true;
+  alarmTime=time;
+  content.classList.add("disable");
+    setAlarmBtn.innerText="Clear Alarm"
+    
+}
+
+setAlarmBtn.addEventListener("click", setAlarm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
